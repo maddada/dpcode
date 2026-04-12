8e52e003c1f07b1e713c31aae26533e019e654c5
Mohamad Yahia <xyzt70@gmail.com>
2026-04-12 09:33:19 +0400

    fix(routes): hide workspace redirect helper from route scan

--- apps/web/src/routes/_chat.workspace.$workspaceId.tsx
+++ apps/web/src/routes/_chat.workspace.$workspaceId.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 
-import { WorkspaceRouteRedirect } from "./workspaceRouteRedirect";
+import { WorkspaceRouteRedirect } from "./-workspaceRouteRedirect";
 
 export const Route = createFileRoute("/_chat/workspace/$workspaceId")({
   component: WorkspaceRouteRedirect,
